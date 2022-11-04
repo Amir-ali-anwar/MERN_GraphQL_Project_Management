dc3f38550fc3c8850e09b629b027f571175e81c8
Amir Ali Anwar <amiralianwar611@gmail.com>
2022-11-04 14:41:14 +0500

    working on connectDb

--- server/app.js
+++ server/app.js
@@ -1,3 +1,21 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./db/connect";
+import "express-async-errors";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.port || 4000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT,()=>{
+        console.log(`server listening on the port ${PORT}`);
+    })
+  } catch (error) {
+    console.log(error);
+  }
+};
