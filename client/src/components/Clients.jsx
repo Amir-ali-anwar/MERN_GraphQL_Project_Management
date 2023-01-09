1ac54b78b05c39b1ee78f2edeb9e767cafe5ba48
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-09 20:30:38 +0500

    working Clinets components

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,4 +1,17 @@
+import {gql, useQuery} from '@apollo/client'
+
+const GET_Clients= gql`
+query getClients {
+  clients{
+    id,
+    name,
+    email,
+    phone
+  }
+}
+`
 const clients = () => {
+  const {data,loading,error}= useQuery(GET_Clients)
   return (
     <div>clients</div>
   )
