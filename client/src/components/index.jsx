23e6d403045e00ae63e733d1464eee8e12341f99
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-19 22:19:01 +0500

    project list and details page completed

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -3,11 +3,12 @@ import AddProjectModal from "./AddProjectModal.jsx";
 import Clients from "./Clients.jsx";
 import Header from "./Header.jsx";
 import Spinner from "./Spinner.jsx";
-
+import Projects from "./Projects.jsx";
 export {
     AddProjectModal,
     Header,
     Clients,
     AddClientModal,
-    Spinner
+    Spinner,
+    Projects
 }
