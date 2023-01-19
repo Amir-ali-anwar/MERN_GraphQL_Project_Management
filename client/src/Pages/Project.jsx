a7474c2cb14229dcbfe8d9f2238d0294ebdaa53b
Amir Ali Anwar <amiralianwar611@gmail.com>
2023-01-19 21:48:45 +0500

    working on the projects module

--- client/src/Pages/Project.jsx
+++ client/src/Pages/Project.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import { Link, useParams } from 'react-router-dom';
+import {Spinner} from '../components/'
+import { useQuery } from '@apollo/client';
+import {GET_PROJECTS} from '../queries/projectQueries'
 const Project = () => {
+  const {id}= useParams()
+  const {loading,data,error}= useQuery(GET_PROJECTS,{variables:{id}})
+  if (loading) return <Spinner />;
+  if (error) return <p>Something Went Wrong</p>;
   return (
     <div>Project</div>
   )
--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -2,9 +2,12 @@ import AddClientModal from "./AddClientModal.jsx";
 import AddProjectModal from "./AddProjectModal.jsx";
 import Clients from "./Clients.jsx";
 import Header from "./Header.jsx";
+import Spinner from "./Spinner.jsx";
+
 export {
     AddProjectModal,
     Header,
     Clients,
-    AddClientModal
+    AddClientModal,
+    Spinner
 }
